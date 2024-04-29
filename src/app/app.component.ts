78d4cedebd002d1de1f5cbcd3932e26187d79aaf
22051988Di <vitalii.soloninko@gmail.com>
2024-04-29 11:16:08 +0200

    v0.0.3	create pages, components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,6 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `
-    <h1>{{ title }}</h1>
-    <button (click)="title = 'new title'">Change title</button>
-    <ul>
-      @for (item of items; track item) {
-      <li>{{ item }}</li>
-      } @if (title === 'new title') {
-      <hr />
-      }
-    </ul>
-  `,
+  template: `<router-outlet />`,
 })
-export class AppComponent {
-  title = 'angular-tg-app!';
-  items = [1, 2, 3, 4, 5];
-}
+export class AppComponent {}
