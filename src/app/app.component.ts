4fcce42dc18bf47f6ca6d715005e8e6188c01ee0
22051988Di <vitalii.soloninko@gmail.com>
2024-04-29 11:05:59 +0200

    v0.0.2	syntax arrays, cycles, conditions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// | uppercase не працює
+//
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: ' <h1>{{title}}</h1>',
+  template: `
+    <h1>{{ title }}</h1>
+    <button (click)="title = 'new title'">Change title</button>
+    <ul>
+      @for (item of items; track item) {
+      <li>{{ item }}</li>
+      } @if (title === 'new title') {
+      <hr />
+      }
+    </ul>
+  `,
 })
 export class AppComponent {
   title = 'angular-tg-app!';
+  items = [1, 2, 3, 4, 5];
 }
